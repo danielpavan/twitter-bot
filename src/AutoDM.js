e6418a0dd707618d06c586e74bb8e5b70287d7f6
danielpavan <40403075+danielpavan@users.noreply.github.com>
2018-06-19 14:52:48 -0300

    UpdateTheMessage
    Update the message

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,8 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Oi ${name} Obrigado por me seguir! \n Gosta de Youtube? Dá uma olhada no meu canal:
+https://www.youtube.com/DanielPavanOficial e Inscreva-se para me ajudar a chegar nos 40mil inscritos! Valeu 😊💙 `; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
